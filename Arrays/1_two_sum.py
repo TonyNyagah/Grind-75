ca219afd7b1491e029a967470628bc9b844d40ee
Antony Nyagah <tony.m.nyagah@gmail.com>
2022-10-21 05:31:57 +0300

    two sum solution

---
+++ Arrays/1_two_sum.py
@@ -0,0 +1,15 @@
+class Solution:
+    def __init__(self, nums: list[int], target: int):
+        self.nums = nums
+        self.target = target
+
+    def twoSum(nums, target) -> list[int]:
+        hash_map = {}
+        for i in range(len(nums)):
+            if nums[i] in hash_map:
+                return [i, hash_map[nums[i]]]
+            else:
+                hash_map[target - nums[i]] = i
+
+
+print(Solution.twoSum(nums=[3, 5, 7, 8, 1, 2], target=15))

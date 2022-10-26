52ec1ecbec83a4735048017ea1b0951d24cddbd1
Antony Nyagah <tony.m.nyagah@gmail.com>
2022-10-26 05:04:56 +0300

    python binary search rewrite

--- Binary Search/704 Binary Search/704_binary_search.py
+++ Binary Search/704 Binary Search/704_binary_search.py
@@ -6,16 +6,16 @@ class Solution:
     def search(nums, target) -> int:
         low = 0
         high = len(nums) - 1
-        mid = 0
 
         while low <= high:
-            mid = low + high
-            if nums[mid] < target:
-                low = mid + 1
-            elif nums[mid] > target:
+            mid = (low + high) // 2
+            guess = nums[mid]
+            if guess == target:
+                return mid
+            if guess > target:
                 high = mid - 1
             else:
-                return mid
+                low = mid + 1
 
         return -1
 
